508ae86df118d9f6497d4fd44219fcd2d78615b6
SrVariable <rojohnibana@gmail.com>
2025-02-19 12:44:23 +0100

    Add support for light theme

--- sorombra.vim
+++ sorombra.vim
@@ -16,14 +16,15 @@ if &background == 'dark'
     hi Normal ctermbg=235 ctermfg=253 guibg=#222222 guifg=#dddddd
     hi ColorColumn cterm=NONE ctermbg=236 ctermfg=NONE guibg=#333333 guifg=NONE
     hi CursorColumn cterm=NONE ctermbg=236 ctermfg=NONE guibg=#333333 guifg=NONE
+    hi Cursor ctermbg=253 ctermfg=235 guibg=#dddddd guifg=#222222
     hi CursorLine cterm=NONE ctermbg=236 ctermfg=NONE guibg=#333333 guifg=NONE
+    hi CursorLineNr cterm=bold ctermbg=236 ctermfg=221 guibg=#333333 guifg=#f2ca51
+    hi LineNr ctermbg=235 ctermfg=242 guibg=#222222 guifg=#666666
     hi Comment cterm=italic ctermfg=141 guifg=#aa85f2
     hi Conceal ctermbg=235 ctermfg=253 guibg=#222222 guifg=#dddddd
     hi Constant cterm=italic ctermfg=253 guifg=#dddddd
     hi Boolean cterm=italic ctermfg=247 guifg=#999999
     hi Number cterm=italic ctermfg=247 guifg=#999999
-    hi Cursor ctermbg=253 ctermfg=235 guibg=#dddddd guifg=#222222
-    hi CursorLineNr cterm=bold ctermbg=236 ctermfg=221 guibg=#333333 guifg=#f2ca51
     hi Delimiter ctermfg=253 guifg=#dddddd
     hi DiffAdd ctermbg=77 ctermfg=235 guibg=#51f26c guifg=#222222
     hi DiffChange ctermbg=221 ctermfg=235 guibg=#f2ca51 guifg=#222222
@@ -39,11 +40,10 @@ if &background == 'dark'
     hi Identifier cterm=NONE ctermfg=221 guifg=#f2ca51
     hi Ignore ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
     hi IncSearch cterm=reverse ctermfg=NONE gui=reverse guifg=NONE term=reverse
-    hi LineNr ctermbg=235 ctermfg=242 guibg=#222222 guifg=#666666
     hi MatchParen cterm=italic ctermbg=NONE ctermfg=203 guibg=NONE guifg=#f25151
     hi ModeMsg ctermfg=253 guifg=#dddddd
     hi MoreMsg ctermfg=253 guifg=#dddddd
-    hi PreProc ctermfg=250 guifg=#b3b3b3
+    hi PreProc ctermfg=251 guifg=#cccccc
     hi Question ctermfg=77 guifg=#51f26c
     hi QuickFixLine ctermbg=215 ctermfg=235 guibg=#f29951 guifg=#222222
     hi Search ctermbg=221 ctermfg=235 guibg=#f2ca51 guifg=#222222
@@ -65,4 +65,55 @@ if &background == 'dark'
     hi FoldColumn ctermbg=238 ctermfg=NONE guibg=#4d4d4d guifg=NONE
     hi link makeCommands Normal
     hi link sqlKeyword sqlStatement
+else
+    hi Normal ctermbg=253 ctermfg=235 guibg=#dddddd guifg=#222222
+    hi ColorColumn cterm=NONE ctermbg=251 ctermfg=NONE guibg=#cccccc guifg=NONE
+    hi CursorColumn cterm=NONE ctermbg=251 ctermfg=NONE guibg=#cccccc guifg=NONE
+    hi Cursor ctermbg=253 ctermfg=253 guibg=#dddddd guifg=#dddddd
+    hi CursorLine cterm=NONE ctermbg=251 ctermfg=NONE guibg=#cccccc guifg=NONE
+    hi CursorLineNr cterm=bold ctermbg=251 ctermfg=214 guibg=#cccccc guifg=#d7af00
+    hi LineNr ctermbg=253 ctermfg=247 guibg=#dddddd guifg=#999999
+    hi Comment cterm=italic ctermfg=141 guifg=#aa85f2
+    hi Conceal ctermbg=235 ctermfg=253 guibg=#222222 guifg=#dddddd
+    hi Constant cterm=italic ctermfg=235 guifg=#222222
+    hi Boolean cterm=italic ctermfg=238 guifg=#4d4d4d
+    hi Number cterm=italic ctermfg=238 guifg=#4d4d4d
+    hi Delimiter ctermfg=253 guifg=#dddddd
+    hi DiffAdd ctermbg=77 ctermfg=235 guibg=#51f26c guifg=#222222
+    hi DiffChange ctermbg=221 ctermfg=235 guibg=#f2ca51 guifg=#222222
+    hi DiffDelete cterm=NONE ctermbg=203 ctermfg=253 guibg=#f25151 guifg=#dddddd
+    hi DiffText cterm=NONE ctermbg=203 ctermfg=253 guibg=#f25151 guifg=#dddddd
+    hi Directory ctermfg=75 guifg=#5194f2
+    hi Error ctermbg=203 ctermfg=253 guibg=#f25151 guifg=#dddddd
+    hi ErrorMsg ctermbg=203 ctermfg=253 guibg=#f25151 guifg=#dddddd
+    hi WarningMsg ctermbg=221 ctermfg=235 guibg=#f2ca51 guifg=#222222
+    hi EndOfBuffer ctermfg=242 guifg=#666666
+    hi NonText ctermfg=242 guifg=#666666
+    hi Function ctermfg=214 guifg=#d7af00
+    hi Identifier cterm=NONE ctermfg=214 guifg=#d7af00
+    hi Ignore ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
+    hi IncSearch cterm=reverse ctermfg=NONE gui=reverse guifg=NONE term=reverse
+    hi MatchParen cterm=italic ctermbg=NONE ctermfg=203 guibg=NONE guifg=#f25151
+    hi ModeMsg ctermfg=235 guifg=#222222
+    hi MoreMsg ctermfg=235 guifg=#222222
+    hi PreProc ctermfg=242 guifg=#666666
+    hi Question ctermfg=70 guifg=#5faf00
+    hi QuickFixLine ctermbg=215 ctermfg=235 guibg=#f29951 guifg=#222222
+    hi Search ctermbg=221 ctermfg=235 guibg=#f2ca51 guifg=#222222
+    hi Special cterm=italic ctermfg=75 guifg=#5194f2
+    hi SpecialKey ctermfg=238 guifg=#4d4d4d
+    hi Statement cterm=bold ctermfg=214 guifg=#d7af00
+    hi StatusLine cterm=reverse ctermbg=236 ctermfg=253 gui=reverse guibg=#333333 guifg=#dddddd term=reverse
+    hi StatusLineTerm cterm=reverse ctermbg=236 ctermfg=253 gui=reverse guibg=#333333 guifg=#dddddd term=reverse
+    hi StatusLineNC cterm=reverse ctermbg=235 ctermfg=253 gui=reverse guibg=#222222 guifg=#dddddd term=reverse
+    hi StatusLineTermNC cterm=reverse ctermbg=235 ctermfg=253 gui=reverse guibg=#222222 guifg=#dddddd term=reverse
+    hi StorageClass ctermfg=214 guifg=#d7af00
+    hi VertSplit ctermbg=236 ctermfg=236 guibg=#333333 guifg=#333333
+    hi Tag cterm=italic ctermfg=75 guifg=#5194f2
+    hi Title cterm=bold ctermfg=235 gui=NONE guifg=#222222
+    hi Todo cterm=bold ctermbg=NONE ctermfg=203 guibg=NONE guifg=#f25151
+    hi Type ctermfg=238 guifg=#4d4d4d
+    hi Visual ctermbg=247 ctermfg=NONE guibg=#999999 guifg=NONE
+    hi Folded ctermbg=247 ctermfg=NONE guibg=#999999 guifg=NONE
+    hi FoldColumn ctermbg=247 ctermfg=NONE guibg=#999999 guifg=NONE
 endif
--- themegen.py
+++ themegen.py
@@ -4,8 +4,10 @@ BLACK = {"cterm": "235", "gui": "#222222"}
 WHITE = {"cterm": "253", "gui": "#dddddd"}
 RED = {"cterm": "203", "gui": "#f25151"}
 YELLOW = {"cterm": "221", "gui": "#f2ca51"}
+DARKYELLOW = {"cterm": 214, "gui": "#d7af00"}
 ORANGE = {"cterm": "215", "gui": "#f29951"}
 GREEN = {"cterm": "77", "gui": "#51f26c"}
+DARKGREEN = {"cterm": "70", "gui": "#5faf00"}
 CYAN = {"cterm": "50", "gui": "#51f2d7"}
 BLUE = {"cterm": "75", "gui": "#5194f2"}
 PURPLE = {"cterm": "141", "gui": "#aa85f2"}
@@ -14,7 +16,7 @@ DARKGRAY = {"cterm": "236", "gui": "#333333"}
 SHADOWGRAY = {"cterm": "238", "gui": "#4d4d4d"}
 GRAY = {"cterm": "242", "gui": "#666666"}
 LIGHTGRAY = {"cterm": "247", "gui": "#999999"}
-SILVERGRAY = {"cterm": "250", "gui": "#b3b3b3"}
+SILVERGRAY = {"cterm": "251", "gui": "#cccccc"}
 
 def header(filename: str = "generated.vim",
            maintainer: str = "SrVariable",
@@ -38,20 +40,19 @@ let g:colors_name = "{filename[:-4]}"
 
 '''
 
-# General highlighting for dark theme, I might add specific highlighting
-# for different filetypes in the future
 def dark_general() -> str:
     return f'''hi Normal ctermbg={BLACK["cterm"]} ctermfg={WHITE["cterm"]} guibg={BLACK["gui"]} guifg={WHITE["gui"]}
     hi ColorColumn cterm=NONE ctermbg={DARKGRAY["cterm"]} ctermfg=NONE guibg={DARKGRAY["gui"]} guifg=NONE
     hi CursorColumn cterm=NONE ctermbg={DARKGRAY["cterm"]} ctermfg=NONE guibg={DARKGRAY["gui"]} guifg=NONE
+    hi Cursor ctermbg={WHITE["cterm"]} ctermfg={BLACK["cterm"]} guibg={WHITE["gui"]} guifg={BLACK["gui"]}
     hi CursorLine cterm=NONE ctermbg={DARKGRAY["cterm"]} ctermfg=NONE guibg={DARKGRAY["gui"]} guifg=NONE
+    hi CursorLineNr cterm=bold ctermbg={DARKGRAY["cterm"]} ctermfg={YELLOW["cterm"]} guibg={DARKGRAY["gui"]} guifg={YELLOW["gui"]}
+    hi LineNr ctermbg={BLACK["cterm"]} ctermfg={GRAY["cterm"]} guibg={BLACK["gui"]} guifg={GRAY["gui"]}
     hi Comment cterm=italic ctermfg={PURPLE["cterm"]} guifg={PURPLE["gui"]}
     hi Conceal ctermbg={BLACK["cterm"]} ctermfg={WHITE["cterm"]} guibg={BLACK["gui"]} guifg={WHITE["gui"]}
     hi Constant cterm=italic ctermfg={WHITE["cterm"]} guifg={WHITE["gui"]}
     hi Boolean cterm=italic ctermfg={LIGHTGRAY["cterm"]} guifg={LIGHTGRAY["gui"]}
     hi Number cterm=italic ctermfg={LIGHTGRAY["cterm"]} guifg={LIGHTGRAY["gui"]}
-    hi Cursor ctermbg={WHITE["cterm"]} ctermfg={BLACK["cterm"]} guibg={WHITE["gui"]} guifg={BLACK["gui"]}
-    hi CursorLineNr cterm=bold ctermbg={DARKGRAY["cterm"]} ctermfg={YELLOW["cterm"]} guibg={DARKGRAY["gui"]} guifg={YELLOW["gui"]}
     hi Delimiter ctermfg={WHITE["cterm"]} guifg={WHITE["gui"]}
     hi DiffAdd ctermbg={GREEN["cterm"]} ctermfg={BLACK["cterm"]} guibg={GREEN["gui"]} guifg={BLACK["gui"]}
     hi DiffChange ctermbg={YELLOW["cterm"]} ctermfg={BLACK["cterm"]} guibg={YELLOW["gui"]} guifg={BLACK["gui"]}
@@ -67,7 +68,6 @@ def dark_general() -> str:
     hi Identifier cterm=NONE ctermfg={YELLOW["cterm"]} guifg={YELLOW["gui"]}
     hi Ignore ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
     hi IncSearch cterm=reverse ctermfg=NONE gui=reverse guifg=NONE term=reverse
-    hi LineNr ctermbg={BLACK["cterm"]} ctermfg={GRAY["cterm"]} guibg={BLACK["gui"]} guifg={GRAY["gui"]}
     hi MatchParen cterm=italic ctermbg=NONE ctermfg={RED["cterm"]} guibg=NONE guifg={RED["gui"]}
     hi ModeMsg ctermfg={WHITE["cterm"]} guifg={WHITE["gui"]}
     hi MoreMsg ctermfg={WHITE["cterm"]} guifg={WHITE["gui"]}
@@ -98,12 +98,65 @@ def dark_makefile() -> str:
 def dark_sql() -> str:
     return f'''hi link sqlKeyword sqlStatement'''
 
-# NOTE: Light is currently unsupported. I might support it in the future.
+def light_general() -> str:
+    return f'''hi Normal ctermbg={WHITE["cterm"]} ctermfg={BLACK["cterm"]} guibg={WHITE["gui"]} guifg={BLACK["gui"]}
+    hi ColorColumn cterm=NONE ctermbg={SILVERGRAY["cterm"]} ctermfg=NONE guibg={SILVERGRAY["gui"]} guifg=NONE
+    hi CursorColumn cterm=NONE ctermbg={SILVERGRAY["cterm"]} ctermfg=NONE guibg={SILVERGRAY["gui"]} guifg=NONE
+    hi Cursor ctermbg={WHITE["cterm"]} ctermfg={WHITE["cterm"]} guibg={WHITE["gui"]} guifg={WHITE["gui"]}
+    hi CursorLine cterm=NONE ctermbg={SILVERGRAY["cterm"]} ctermfg=NONE guibg={SILVERGRAY["gui"]} guifg=NONE
+    hi CursorLineNr cterm=bold ctermbg={SILVERGRAY["cterm"]} ctermfg={DARKYELLOW["cterm"]} guibg={SILVERGRAY["gui"]} guifg={DARKYELLOW["gui"]}
+    hi LineNr ctermbg={WHITE["cterm"]} ctermfg={LIGHTGRAY["cterm"]} guibg={WHITE["gui"]} guifg={LIGHTGRAY["gui"]}
+    hi Comment cterm=italic ctermfg={PURPLE["cterm"]} guifg={PURPLE["gui"]}
+    hi Conceal ctermbg={BLACK["cterm"]} ctermfg={WHITE["cterm"]} guibg={BLACK["gui"]} guifg={WHITE["gui"]}
+    hi Constant cterm=italic ctermfg={BLACK["cterm"]} guifg={BLACK["gui"]}
+    hi Boolean cterm=italic ctermfg={SHADOWGRAY["cterm"]} guifg={SHADOWGRAY["gui"]}
+    hi Number cterm=italic ctermfg={SHADOWGRAY["cterm"]} guifg={SHADOWGRAY["gui"]}
+    hi Delimiter ctermfg={WHITE["cterm"]} guifg={WHITE["gui"]}
+    hi DiffAdd ctermbg={GREEN["cterm"]} ctermfg={BLACK["cterm"]} guibg={GREEN["gui"]} guifg={BLACK["gui"]}
+    hi DiffChange ctermbg={YELLOW["cterm"]} ctermfg={BLACK["cterm"]} guibg={YELLOW["gui"]} guifg={BLACK["gui"]}
+    hi DiffDelete cterm=NONE ctermbg={RED["cterm"]} ctermfg={WHITE["cterm"]} guibg={RED["gui"]} guifg={WHITE["gui"]}
+    hi DiffText cterm=NONE ctermbg={RED["cterm"]} ctermfg={WHITE["cterm"]} guibg={RED["gui"]} guifg={WHITE["gui"]}
+    hi Directory ctermfg={BLUE["cterm"]} guifg={BLUE["gui"]}
+    hi Error ctermbg={RED["cterm"]} ctermfg={WHITE["cterm"]} guibg={RED["gui"]} guifg={WHITE["gui"]}
+    hi ErrorMsg ctermbg={RED["cterm"]} ctermfg={WHITE["cterm"]} guibg={RED["gui"]} guifg={WHITE["gui"]}
+    hi WarningMsg ctermbg={YELLOW["cterm"]} ctermfg={BLACK["cterm"]} guibg={YELLOW["gui"]} guifg={BLACK["gui"]}
+    hi EndOfBuffer ctermfg={GRAY["cterm"]} guifg={GRAY["gui"]}
+    hi NonText ctermfg={GRAY["cterm"]} guifg={GRAY["gui"]}
+    hi Function ctermfg={DARKYELLOW["cterm"]} guifg={DARKYELLOW["gui"]}
+    hi Identifier cterm=NONE ctermfg={DARKYELLOW["cterm"]} guifg={DARKYELLOW["gui"]}
+    hi Ignore ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
+    hi IncSearch cterm=reverse ctermfg=NONE gui=reverse guifg=NONE term=reverse
+    hi MatchParen cterm=italic ctermbg=NONE ctermfg={RED["cterm"]} guibg=NONE guifg={RED["gui"]}
+    hi ModeMsg ctermfg={BLACK["cterm"]} guifg={BLACK["gui"]}
+    hi MoreMsg ctermfg={BLACK["cterm"]} guifg={BLACK["gui"]}
+    hi PreProc ctermfg={GRAY["cterm"]} guifg={GRAY["gui"]}
+    hi Question ctermfg={DARKGREEN["cterm"]} guifg={DARKGREEN["gui"]}
+    hi QuickFixLine ctermbg={ORANGE["cterm"]} ctermfg={BLACK["cterm"]} guibg={ORANGE["gui"]} guifg={BLACK["gui"]}
+    hi Search ctermbg={YELLOW["cterm"]} ctermfg={BLACK["cterm"]} guibg={YELLOW["gui"]} guifg={BLACK["gui"]}
+    hi Special cterm=italic ctermfg={BLUE["cterm"]} guifg={BLUE["gui"]}
+    hi SpecialKey ctermfg={SHADOWGRAY["cterm"]} guifg={SHADOWGRAY["gui"]}
+    hi Statement cterm=bold ctermfg={DARKYELLOW["cterm"]} guifg={DARKYELLOW["gui"]}
+    hi StatusLine cterm=reverse ctermbg={DARKGRAY["cterm"]} ctermfg={WHITE["cterm"]} gui=reverse guibg={DARKGRAY["gui"]} guifg={WHITE["gui"]} term=reverse
+    hi StatusLineTerm cterm=reverse ctermbg={DARKGRAY["cterm"]} ctermfg={WHITE["cterm"]} gui=reverse guibg={DARKGRAY["gui"]} guifg={WHITE["gui"]} term=reverse
+    hi StatusLineNC cterm=reverse ctermbg={BLACK["cterm"]} ctermfg={WHITE["cterm"]} gui=reverse guibg={BLACK["gui"]} guifg={WHITE["gui"]} term=reverse
+    hi StatusLineTermNC cterm=reverse ctermbg={BLACK["cterm"]} ctermfg={WHITE["cterm"]} gui=reverse guibg={BLACK["gui"]} guifg={WHITE["gui"]} term=reverse
+    hi StorageClass ctermfg={DARKYELLOW["cterm"]} guifg={DARKYELLOW["gui"]}
+    hi VertSplit ctermbg={DARKGRAY["cterm"]} ctermfg={DARKGRAY["cterm"]} guibg={DARKGRAY["gui"]} guifg={DARKGRAY["gui"]}
+    hi Tag cterm=italic ctermfg={BLUE["cterm"]} guifg={BLUE["gui"]}
+    hi Title cterm=bold ctermfg={BLACK["cterm"]} gui=NONE guifg={BLACK["gui"]}
+    hi Todo cterm=bold ctermbg=NONE ctermfg={RED["cterm"]} guibg=NONE guifg={RED["gui"]}
+    hi Type ctermfg={SHADOWGRAY["cterm"]} guifg={SHADOWGRAY["gui"]}
+    hi Visual ctermbg={LIGHTGRAY["cterm"]} ctermfg=NONE guibg={LIGHTGRAY["gui"]} guifg=NONE
+    hi Folded ctermbg={LIGHTGRAY["cterm"]} ctermfg=NONE guibg={LIGHTGRAY["gui"]} guifg=NONE
+    hi FoldColumn ctermbg={LIGHTGRAY["cterm"]} ctermfg=NONE guibg={LIGHTGRAY["gui"]} guifg=NONE'''
+
 def color() -> str:
     return f'''if &background == \'dark\'
     {dark_general()}
     {dark_makefile()}
     {dark_sql()}
+else
+    {light_general()}
 endif
 '''
 
